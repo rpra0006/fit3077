2093b898ddd0630449fbe1e22809a12e18243078
Jason <jagu0001@student.monash.edu>
2020-05-04 23:31:30 +1000

    Implemented method for getting all patients from a given practitioner ID; using _id instead of identifier for now

--- src/FhirApiAdapter.java
+++ src/FhirApiAdapter.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Encounter;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
 
@@ -8,9 +11,10 @@ import ca.uhn.fhir.parser.*;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 public class FhirApiAdapter extends FhirServer {
-	private final String BASE_URL = "https://fhir.monash.edu/hapi-fhir-jpaserver/fhir";
-	private IParser parser = FhirContext.forR4().newJsonParser().setPrettyPrint(true);
-	private IGenericClient client = FhirContext.forR4().newRestfulGenericClient(BASE_URL);
+	private final String BASE_URL = "https://fhir.monash.edu/hapi-fhir-jpaserver/fhir/";
+	private FhirContext ctx = FhirContext.forR4();
+	private IParser parser = ctx.newJsonParser().setPrettyPrint(true);
+	private IGenericClient client = ctx.newRestfulGenericClient(BASE_URL);
 	
 	@Override
 	public Patient getPatient(String patientID) {
@@ -23,7 +27,27 @@ public class FhirApiAdapter extends FhirServer {
 	@Override
 	public ArrayList<Patient> getAllPractitionerPatients(String practitionerID) {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Patient> patientList = new ArrayList<Patient>();
+		
+		
+		Bundle encounters = client.search()
+				.forResource(Encounter.class)
+				.where(Encounter.PRACTITIONER.hasId(practitionerID))
+				.returnBundle(Bundle.class).execute();
+		
+		System.out.println(parser.encodeResourceToString(encounters));
+		
+		for(BundleEntryComponent entry: encounters.getEntry()) {
+			Encounter encounter = (Encounter) entry.getResource();
+			String patientURL = encounter.getSubject().getReference();
+			System.out.println(patientURL);
+			
+			Patient patient = client.read().resource(Patient.class).withUrl(BASE_URL + patientURL).execute();
+			patientList.add(patient);
+		}
+		
+		//REMOVE DUPLICATES
+		return patientList;
 	}
 
 	@Override
